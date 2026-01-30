3ea7f682f9dfb13d9bc788d130641c22a117d14d
Murilo P. S <mu-silva@outlook.com>
2026-01-29 21:28:06 -0300

    chore: remove unused modules and cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,7 @@ rootProject.name = "somnia"
 
 include("somnia-lang")
 include("somnia-engine")
-// include("somnia-lsp")
-// include("somnia-cli")
-// include("somnia-demo-mvp")
+
 include("somnia-runtime")
 include("somnia-boot-starter")
 include("somnia-crud-app")
